be1d7b95f5c88f7fc9435dec33ca74e9b6b00f95
ionovm376 <marat.ionov0192878@gmail.com>
2025-12-25 05:19:43 +0300

    Update Header.h

--- lab8/Header.h
+++ lab8/Header.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#ifdef STDDLL_EXPORTS  // или MYLIBRARY_EXPORTS
+#ifdef STDDLL_EXPORTS  
 #define STDDLL_API __declspec(dllexport)
 #else
 #define STDDLL_API __declspec(dllimport)
@@ -8,4 +8,5 @@
 
 extern "C" {
     STDDLL_API double num_even_vals_in_arr(const int arr[], int size);
+
 }
